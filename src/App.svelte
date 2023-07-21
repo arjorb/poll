8fb746dc3e20c8cb737021d0b54cab7eb2796acf
John U <arjorb@gmail.com>
2023-07-21 17:13:50 +0200

    use the bind and value keywords

--- src/App.svelte
+++ src/App.svelte
@@ -12,9 +12,9 @@ const handleColor  = (e) =>{
 
 
 <main>
-	<h1 style={"color:"+newColor}>Hello {name}!</h1>
+	<h1 style="color:{newColor}">Hello {name}!</h1>
 	<p>{newColor}</p>
-	<input type="text" on:input={(e)=>handleColor(e)}  value={newColor}>
+	<input type="text" bind:value={newColor}>
 </main>
 
 <style>
