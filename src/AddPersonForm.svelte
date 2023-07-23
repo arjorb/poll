61766baf305338be7371390231d558e2350e7047
John U <arjorb@gmail.com>
2023-07-23 21:20:49 +0200

    implement handleForm function and use the  event modifier on the form submit

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -1,8 +1,15 @@
-<script></script>
+<script>
+    let name;
+    let age;
+
+    const handleForm = () =>{
+        console.log(`The name is ${name} and I'm ${age} years old`)
+    }
+</script>
 <main>
-    <form action="">
-        <input type="text" placeholder="Enter the name">
-        <input type="text" placeholder="Enter the ages">
+    <form on:submit|preventDefault={handleForm}>
+        <input type="text" placeholder="Enter the name" bind:value={name}>
+        <input type="number" placeholder="Enter the ages" bind:value={age}>
         <button>Add Person</button>
     </form>
 </main>
