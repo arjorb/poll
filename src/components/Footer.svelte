63a862ca9fa45005d95bd58f1f50328713ae8ac3
John U <arjorb@gmail.com>
2023-07-24 00:13:56 +0200

    creating a footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,13 @@
+<footer>
+    <div class="copyright">Copyright @ 2023 - Poll</div>
+</footer>
+
+<style>
+    footer{
+        text-align: center;
+    }
+    .copyright{
+        color: #aaa;
+        border-top: 1px solid #ddd;
+    }
+</style>
