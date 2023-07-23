d2633efa57a69592400fde117f6ab4590e37ea86
John U <arjorb@gmail.com>
2023-07-23 22:35:53 +0200

    creating a custom event dispatcher

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -1,28 +1,23 @@
 <script>
+  import { createEventDispatcher } from "svelte/types/runtime/internal/lifecycle";
+
+    const dispatch  = createEventDispatcher();
     let name;
     let age;
-    let beltLevel ;
-    let skills =  []
 
     const handleForm = () =>{
-        console.log(`The name is ${name} and I'm ${age} years old, I have a ${beltLevel}  belt`, skills, )
+        const person = {
+            name,
+            age,
+            id: Math.random()
+        }
+        dispatch("addPerson", person);
     }
 </script>
 <main>
     <form on:submit|preventDefault={handleForm}>
         <input type="text" placeholder="Enter the name" bind:value={name}>
         <input type="number" placeholder="Enter the ages" bind:value={age}>
-        <label for="">Skills</label>
-        <input type="checkbox" bind:group={skills} value="fighting"/> fighting <br>
-        <input type="checkbox" bind:group={skills} value="sneaking"> sneaking <br>
-        <input type="checkbox" bind:group={skills} value="running"> running <br>
-        <label for="">Belt</label>
-        <select bind:value={beltLevel}>
-            <option value="black">black</option>
-            <option value="marrow">marrow</option>
-            <option value="white">white</option>
-            <option value="orange">orange</option>
-        </select>
         <button>Add Person</button>
     </form>
 </main>
