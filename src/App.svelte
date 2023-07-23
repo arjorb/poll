b2a2ff530e6eb1a68de570f47da45fc647489c41
John U <arjorb@gmail.com>
2023-07-23 23:54:19 +0200

    initializing the poll project

--- src/App.svelte
+++ src/App.svelte
@@ -1,75 +1,10 @@
 <script>
-  import AddPersonForm from "./AddPersonForm.svelte";
-import Modal from "./Modal.svelte";
-	
-	let name =  "John";
-	let newColor = '';
-	let showModal = false;
-
-	let people = [{name:'John',age:11, id:1},{name:'Aldo',age:1, id:2},{name:'Aline',age:2, id:3},{name:'Nkaka',age:1}]
-
-	const  handleName = ()  =>{
-		name = "Arjo"
-	}
-
-
-	const handleDelete = (id) =>{
-		people = people.filter(person => person.id !== id);
-	}
-
-	const handlePromo = () => {
-		showModal = !showModal
-	}
-
-	const addPerson = (e)  =>{
-		people  =  [e.detail,  ...people];
-		showModal= false;
-	}
-
 </script>
 
 <main>
-	{#if showModal}
-	<Modal  isPromo={true} on:click={handlePromo}>
-	<h3>Add New Person</h3>
-	<AddPersonForm on:addPerson={addPerson}/>
-	</Modal>
-	{/if}
-	<h1 style="color:{newColor}">Hello {name}!</h1>
-	<button on:click={handlePromo}>Promotion is Here</button>
-	<p>{newColor}</p>
-	<input type="text" bind:value={newColor}>
-
-	{#each people as person }
-		<h1 style="color:{newColor}">{person.name}</h1>
-		{#if person.age > 10}
-		<p>Allowed to drink Juice</p>
-		{/if}
-		<h1 style="color:{newColor}">{person.age}</h1>
-		<button on:click={()=>handleDelete(person.id)}>Delete</button>
-		{:else}
-		<p>There is no any person today</p>
-	{/each}
+	<h1>Hello  world!</h1>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
