0d27c2a467c8cda4ef2dce5a5cad22c8d1f53483
John U <arjorb@gmail.com>
2023-07-23 22:16:02 +0200

    implement the select and option with bind value

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -1,10 +1,11 @@
 <script>
     let name;
     let age;
+    let beltLevel ;
     let skills =  []
 
     const handleForm = () =>{
-        console.log(`The name is ${name} and I'm ${age} years old`, skills )
+        console.log(`The name is ${name} and I'm ${age} years old, I have a ${beltLevel}  belt`, skills, )
     }
 </script>
 <main>
@@ -15,6 +16,13 @@
         <input type="checkbox" bind:group={skills} value="fighting"/> fighting <br>
         <input type="checkbox" bind:group={skills} value="sneaking"> sneaking <br>
         <input type="checkbox" bind:group={skills} value="running"> running <br>
+        <label for="">Belt</label>
+        <select bind:value={beltLevel}>
+            <option value="black">black</option>
+            <option value="marrow">marrow</option>
+            <option value="white">white</option>
+            <option value="orange">orange</option>
+        </select>
         <button>Add Person</button>
     </form>
 </main>
