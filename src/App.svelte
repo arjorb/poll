e79eb035f6baaf62375d0c90f88941b6ff0c0974
John U <arjorb@gmail.com>
2023-07-24 00:00:01 +0200

    set max width

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,12 @@
 </script>
 
 <main>
-	<h1>Hello  world!</h1>
+	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni voluptatem a quam consectetur voluptates sint ea, exercitationem quisquam animi nam facilis non molestias possimus dolore tenetur mollitia modi! Veritatis iure ut praesentium voluptatibus distinctio quae ipsam quas magnam labore suscipit!</p>
 </main>
 
 <style>
-	
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
 </style>
