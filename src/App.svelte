b9173dfcbba1ff5d59967986b84445bde710a85e
John U <arjorb@gmail.com>
2023-07-26 11:01:22 +0200

    loop of the pools and display them

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,48 @@
 <script>
-  import Footer from "./components/Footer.svelte";
-  import Header from "./components/Header.svelte";
-  import AddNewPollForm from "./components/addNewPollForm.svelte";
-  import Tabs from "./components/shared/Tabs.svelte";
+  import Footer from './components/Footer.svelte';
+  import Header from './components/Header.svelte';
+  import PollList from './components/PollList.svelte';
+  import AddNewPollForm from './components/addNewPollForm.svelte';
+  import Tabs from './components/shared/Tabs.svelte';
 
-
-  const items =  ['Current Polls', 'Add New Polls'];
+  const items = ['Current Polls', 'Add New Polls'];
   let activeItem = 'Current Polls';
-  let  polls = [{question: 'Pyton or JavaScript', answerA: 'JavaScript', answerB: 'Pyton', voteA:10, voteB:10, id: 1}]
+  let polls = [
+    {
+      question: 'Pyton or JavaScript',
+      answerA: 'JavaScript',
+      answerB: 'Pyton',
+      voteA: 10,
+      voteB: 10,
+      id: 1,
+    },
+  ];
 
+  const handleTabs = e => {
+    activeItem = e.detail;
+  };
 
-  const  handleTabs  = (e) =>{
-	activeItem = e.detail
-  }
-
-  const handleAddNewPool = (e) =>{
-	polls = [e.detail,  ...polls]
-	console.log('From App Component:',polls)
-	activeItem = 'Current Polls'
-  }
+  const handleAddNewPool = e => {
+    polls = [e.detail, ...polls];
+    console.log('From App Component:', polls);
+    activeItem = 'Current Polls';
+  };
 </script>
 
-<Header/>
+<Header />
 <main>
-	<Tabs {items} {activeItem} on:changeTab={handleTabs}/>
-	{#if activeItem  === 'Current Polls'}
-	<p>This is a page of the Current Pools</p>
-	{:else if activeItem === 'Add New Polls'}
-	<AddNewPollForm on:add={handleAddNewPool}/>
-	{/if}
+  <Tabs {items} {activeItem} on:changeTab={handleTabs} />
+  {#if activeItem === 'Current Polls'}
+    <PollList {polls} />
+  {:else if activeItem === 'Add New Polls'}
+    <AddNewPollForm on:add={handleAddNewPool} />
+  {/if}
 </main>
-<Footer/>
+<Footer />
+
 <style>
-	main{
-		max-width: 960px;
-		margin: 40px auto;
-	}
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,3 +1,11 @@
-<script></script>
-<main></main>
+<script>
+  export let polls = [];
+</script>
+
+<main>
+  {#each polls as poll}
+    <p>{poll.question}</p>
+  {/each}
+</main>
+
 <style></style>
